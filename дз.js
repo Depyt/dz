8bc2a02d34b5ebad652129624f1ba53607066a58
Depyt <73955929+Depyt@users.noreply.github.com>
2021-02-06 17:21:40 +0300

    Add files via upload

---
+++ дз.js
@@ -0,0 +1,25 @@
+//let i = 1;
+//while (i <= 100){
+//   document.write(i + '<br>'); 
+// i++;}
+
+//for (let i = 1; i <=100 ; i++ ){
+//    document.write(i + '<br>');}
+
+//for (let i = 11; i <=33 ; i++){
+//    document.write(i + '<br>')}
+
+//let result = 1;
+//let arr = [1, 2, 3, 4, 5,];
+//for (let i = 0; i < arr.length; i++ ){
+//    result = result + arr[i];}
+//alert(result)
+
+//let obj = {
+//    green :'зеленый',
+//    red :'красный',
+//    blue :'голубой'};
+//for (let key in obj){
+//    alert(key + ' - это ' + obj[key] + ' .' );};
+
+
